2ee473cbc9de0fbbafaae2328248e4e42648232b
kateMoscoso <katiemscs@gmail.com>
2019-12-03 08:59:49 +0100

    first implementation

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();;
+
+app.get('/', (req, res)=> {
+    res.send('Hello World')
+})
+
+app.get('/api/courses', (req, res) =>{
+    res.send([1,2,3])
+})
+app.get('/api/courses/:id', (req, res) =>{
+    res.send(req.params.id)
+})
+app.get('/api/post/:year/:month', (req, res) =>{
+    //req.query
+    res.send(req.params)
+})
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening port ${3000}`))
+//export PORT 5000
