02103bfc87316869c1db866da000ebe29d62b84b
kateMoscoso <katiemscs@gmail.com>
2020-08-04 12:54:02 +0200

    more test and coverage test

--- utils/buildMessage.js
+++ utils/buildMessage.js
@@ -1,8 +1,8 @@
-function buildMessage(entity, action){
-    if(action === 'list'){
-        return `${entity}s listed`;
-    }
-    return `${entity} ${action}d`;
+function buildMessage(entity, action) {
+  if (action === "list") {
+    return `${entity}s listed`;
+  }
+  return `${entity} ${action}d`;
 }
 
 module.exports = buildMessage;
